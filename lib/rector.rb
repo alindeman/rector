41dab10d06e798e289ddfdd0a221a7fb07b6ed6f
Andy Lindeman <andy@highgroove.com>
2012-02-06 12:02:57 -0500

    Spikes on some "differencer" classes that can tell what changed from one object to another

--- lib/rector.rb
+++ lib/rector.rb
@@ -4,6 +4,7 @@ require "redis-namespace"
 require_relative "rector/configuration"
 require_relative "rector/worker"
 require_relative "rector/backends"
+require_relative "rector/data"
 
 module Rector
   class << self
